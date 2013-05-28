34f0abe2f6a66e61e600835a4a54fcb4810bb7e6
HawkOwl <hawkowl@outlook.com>
2013-05-28 18:50:21 +0800

    Updating setup version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 	name = "eagleeye",
-	version = "0.1.0",
+	version = "0.2.0",
 	author = "HawkOwl",
 	author_email = "hawkowl@outlook.com",
 	description = "EagleEye is a library for metrics reporting, using Twisted.",
